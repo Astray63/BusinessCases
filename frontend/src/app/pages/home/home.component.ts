a67cc378d377b398d0e005a1aee4d83321412653
toji-42 <faruksulvac@gmail.com>
2025-11-16 22:32:42 +0100

    main page animation reparation

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -9,6 +9,7 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   private map!: L.Map;
