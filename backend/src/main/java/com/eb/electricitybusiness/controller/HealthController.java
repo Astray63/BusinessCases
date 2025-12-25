668057aaa22fda8b24c6ab58ec6eef069aba20ef
Astray63 <astray63000@gmail.com>
2025-12-25 13:32:31 +0100

    deploy debug

---
+++ backend/src/main/java/com/eb/electricitybusiness/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.eb.electricitybusiness.controller;
+
+import com.eb.electricitybusiness.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<String>> checkHealth() {
+        return ResponseEntity.ok(ApiResponse.success("Backend is running!"));
+    }
+}
